1a6a516bb28a8679a9a166b42f499a6366e4ff2c
Olafur <olafuringieinarsson@gmail.com>
2021-11-30 20:59:15 +0100

    setup html

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-decodeURI
+// Set up our express.js server
+const express = require('express')
+const app = express()
+const router = express.Router();
+const port = 3000
+ 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`)
+})
+
